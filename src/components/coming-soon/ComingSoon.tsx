202f536fa60b760730c540012c168f32469ca0aa
pankaj0998 <pankajchaurasia95988@gmail.com>
2025-01-22 14:17:03 +0530

    Index file update

--- src/components/coming-soon/ComingSoon.tsx
+++ src/components/coming-soon/ComingSoon.tsx
@@ -12,7 +12,7 @@ const ComingSoon = () => {
                 <div className="absolute top-0 right-0 bottom-0 left-0 bg-gray-900 opacity-75"></div>
 
                 <div className="z-50 flex flex-col justify-center items-center text-white w-full h-screen space-y-10">
-                    <h1 className="text-5xl">We are <b>Almost</b> there!</h1>
+                    <h1 className="text-7xl">Coming Soon...</h1>
                     <p>Stay tuned for something amazing!!!</p>
                 </div>
 
