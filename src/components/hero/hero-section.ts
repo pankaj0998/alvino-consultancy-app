901adce5982ff23f3acc4a922dafc2dbc46d3fab
pankaj0998 <pankajchaurasia95988@gmail.com>
2025-01-25 04:16:32 +0530

    Push

--- src/components/hero/hero-section.ts
+++ src/components/hero/hero-section.ts
@@ -2,7 +2,7 @@ import { urls } from "../../links/url";
 import { HeroSectionProps } from "./Hero.types";
 
 export const heroSection: HeroSectionProps = {
-    heading: "Alvino Consultancy",
+    heading: "Consultancy",
     description: "Alvino Consultancy is a dynamic consulting and accounting firm providing customized solutions to multinational corporations (MNCs), corporate small and medium enterprises (SMEs), and high-net-worth individuals (HNIs) across India region. With a focus on optimizing business operations, ensuring regulatory compliance, and maximizing tax efficiency, we deliver strategic insights that drive growth, mitigate risks, and enhance value. Backed by a network of affiliates/ business partners across key domestic and international markets, we offer integrated solutions designed to help businesses succeed in today’s continuously evolving global marketplace",
     buttonText: "About us",
     route: urls.aboutus
